ea4c64797d7b964a7947660d92d0222aedd112a8
DeeJhay <amdeejhay@gmail.com>
2025-09-03 20:59:05 +0100

    i made changes to the terrafrom files

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" { type = string default = "us-east-1" }
-variable "project" { type = string default = "serverless-ttx" }
-variable "table_name" { type = string default = "serverless-ttx-items" }
-variable "ssm_prefix" { type = string default = "/serverless-ttx/" }
+variable "project" { type = string default = "serverless-terraform" }
+variable "name_table" { type = string default = "serverless-terraform-items" }
+variable "ssm_prefix" { type = string default = "/serverless-terraform/" }
 variable "allowed_cors_origins" { type = list(string) default = ["http://localhost:3000"] }
